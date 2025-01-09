8fe7deeb83ef412a8ad5bb40d5f6d0235dbc1e01
Surelyyy <chienming.tee@gmail.com>
2025-01-09 12:48:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ if st.button("Test Connection"):
         response = requests.get(url, timeout=5, stream=True)
         
         # Check if the response is successful and contains image data
-        if response.status_code == 200:
+        #if response.status_code == 200:
             # Load the image from the response content
-            image = Image.open(io.BytesIO(response.content))
-            st.success("Successfully connected to Misty! Here's the image captured:")
-            st.image(image, caption="Image from Misty's Camera", use_column_width=True)
+        image = Image.open(io.BytesIO(response.content))
+        st.success("Successfully connected to Misty! Here's the image captured:")
+        st.image(image, caption="Image from Misty's Camera", use_column_width=True)
         else:
             st.error(f"Failed to connect. Status code: {response.status_code}")
     except requests.exceptions.RequestException as e:
