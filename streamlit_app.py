0623c5fd6424a77a91ed494bbd163fc857d24346
Surelyyy <chienming.tee@gmail.com>
2025-01-09 12:51:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ url = "http://192.168.0.149/api/cameras/rgb"
 try:
     response = requests.get(url, timeout=5, stream=True)
     if response.status_code == 200:
-        st.print("Connection Successful!")
+        st.show("Connection Successful!")
         image = Image.open(io.BytesIO(response.content))
         image.show()  # Opens the image in the default viewer
     else:
-        st.print(f"Failed to connect. Status code: {response.status_code}")
+        st.error(f"Failed to connect. Status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
-    st.print(f"Connection error: {e}")
+    st.error(f"Connection error: {e}")
